ba7ba0113803f7644849e0bcc92923160da38aaa
DBL <blduong.vn@gmail.com>
2021-11-30 21:28:17 +0700

    final

--- Chager/CustomView/CircleDrag.swift
+++ Chager/CustomView/CircleDrag.swift
@@ -1,8 +1,36 @@
-//
-//  CircleDrag.swift
-//  Chager
-//
-//  Created by Duong Bao Long on 11/30/21.
-//
+import UIKit
 
-import Foundation
+
+
+
+class CircleDrag: UIView {
+    
+    var didLoad = false
+    var dragView = UIView()
+    var button = CAShapeLayer()
+    var minCircle = CAShapeLayer()
+    var maxCircle = CAShapeLayer()
+    var percentageCircle = CAShapeLayer()
+    var text = CATextLayer()
+    var dragRound = CAShapeLayer()
+    
+    override func draw(_ rect: CGRect) {
+        if didLoad {return}
+        didLoad = true
+        
+        self.layer.addSublayer(maxCircle)
+        self.layer.addSublayer(minCircle)
+        self.layer.addSublayer(percentageCircle)
+        dragView.layer.addSublayer(dragRound)
+        dragView.layer.addSublayer(button)
+        self.layer.addSublayer(text)
+        
+        self.addSubview(dragView)
+        dragView.addGestureRecognizer(UIPanGestureRecognizer(target: self, action: #selector(dra))
+    }
+    
+    
+    @objc func drag(_ sender: UIPanGestureRecognizer) {
+        
+    }
+}
